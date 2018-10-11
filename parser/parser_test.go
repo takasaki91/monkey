7e02db7766918b560717a18b9e33deeadfcf2833
takasaki91 <y.baton@gmail.com>
2018-10-11 16:07:03 +0900

    modify parser.parserreturnstatementt

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -76,3 +76,31 @@ func checkParserErrors( t *testing.T, p *Parser)  {
 	}
 	t.FailNow()
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 848483;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements dows not contain 3 stateents. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T",stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",returnStmt.TokenLiteral())
+		}
+	}
+}
