4031304697dc0289f319b155d9465e2d3a28996e
takasaki91 <y.baton@gmail.com>
2018-10-17 14:34:39 +0900

    add String and TestString

--- ast/ast.go
+++ ast/ast.go
@@ -2,11 +2,13 @@ package ast
 
 import (
 	"monkey/token"
+	"bytes"
 )
 
 
 type Node interface{
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -30,6 +32,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -38,6 +49,19 @@ type LetStatement struct {
  
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ") 
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -46,7 +70,10 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {return i.Token.Literal}
- 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 type ReturnStatement struct {
 	Token token.Token
 	ReturnValue Expression
@@ -54,4 +81,28 @@ type ReturnStatement struct {
 
 func (r *ReturnStatement) statementNode() {}
 func (r *ReturnStatement) TokenLiteral() string {return r.Token.Literal}
+func (r *ReturnStatement) String() string {
+	var out bytes.Buffer
 
+	out.WriteString(r.TokenLiteral() + " ") 
+
+	if r.ReturnValue != nil {
+		out.WriteString(r.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode() {}
+func (e *ExpressionStatement) TokenLiteral() string {return e.Token.Literal}
+func (e *ExpressionStatement) String() string {
+	if e.Expression != nil {
+		return e.Expression.String()
+	}
+	return ""
+}
