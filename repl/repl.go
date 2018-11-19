45e2e8c4ba839963ec1c899c16a7ce825381f3b6
takasaki91 <y.baton@gmail.com>
2018-11-19 19:37:11 +0900

    2章が終わった

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"fmt"
 	"monkey/lexer"
+	"monkey/parser"
 	"monkey/token"
 )
 const PROMPT = ">> "
@@ -21,9 +22,24 @@ func Start (in io.Reader, out io.Writer) {
 		
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n",tok)
 		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
