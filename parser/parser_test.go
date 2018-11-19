acc1e481a08f5fddb77f0b50030e8cb56d39d30f
takasaki91 <y.baton@gmail.com>
2018-11-19 10:48:29 +0900

    呼び出し式とそれのテストを追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -408,6 +408,10 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"!(true == true)",
 			"(!(true == true))",
 		},
+		{
+			"a + add(b*c) + d",
+			"((a + add((b * c))) + d)",
+		},
 	}
 	for _, tt := range tests {
 		l := lexer.New(tt.input)
@@ -652,4 +656,39 @@ func TestFunctionParameterParsing(t *testing.T) {
 			testLiteralExpression(t, function.Parameters[i], ident)
 		}
 	}
+}
+func TestCallExpressionParsing(t *testing.T) {
+	input := "add(1, 2*3,4+5)"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t,p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.FunctionalLiteral. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, exp.Function, "add") {
+		return
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("wrong length ofarguemnts.got=%d\n", len(exp.Arguments))
+	}
+	testLiteralExpression(t, exp.Arguments[0],1)
+	testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
+	testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
+	
 }
