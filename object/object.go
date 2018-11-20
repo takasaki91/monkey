fd04c0b87b45f7ce2fa850b4d024c17ea7ec85f6
takasaki91 <y.baton@gmail.com>
2018-11-20 14:24:40 +0900

    objectを追加した

---
+++ object/object.go
@@ -0,0 +1,35 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const ( 
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
+)
+type Object interface {
+	Type() ObjectType
+	Inspct() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ}
+func (i *Integer) Inspct() string { return fmt.Sprintf("%d", i.Value)}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {return BOOLEAN_OBJ }
+func (b *Boolean) Inspct() string { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct {
+
+}
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspct() string { return "null" }
