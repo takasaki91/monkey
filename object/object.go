d527959330b2679c0c463f8050a057ae9a440dbc
takasaki91 <y.baton@gmail.com>
2018-11-20 15:10:05 +0900

    タイポを直した

--- object/object.go
+++ object/object.go
@@ -11,7 +11,7 @@ const (
 )
 type Object interface {
 	Type() ObjectType
-	Inspct() string
+	Inspect() string
 }
 
 type Integer struct {
@@ -19,17 +19,17 @@ type Integer struct {
 }
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ}
-func (i *Integer) Inspct() string { return fmt.Sprintf("%d", i.Value)}
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value)}
 
 type Boolean struct {
 	Value bool
 }
 
 func (b *Boolean) Type() ObjectType {return BOOLEAN_OBJ }
-func (b *Boolean) Inspct() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct {
 
 }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspct() string { return "null" }
+func (n *Null) Inspect() string { return "null" }
