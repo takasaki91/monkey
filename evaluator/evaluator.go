cb44c96b21f9cd19061555243cd5ce9a5e4220e3
takasaki91 <y.baton@gmail.com>
2018-11-20 15:11:15 +0900

    整数リテラルが出るようにした

---
+++ evaluator/evaluator.go
@@ -0,0 +1,27 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node:= node.(type) {
+	case *ast.Program:
+		return EvalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{ Value: node.Value}
+	}
+	return nil
+}
+func EvalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object 
+
+	for _,statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
