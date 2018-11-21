bbf2f8328f679b15f3537b8a72c912ccde3fb8ee
takasaki91 <y.baton@gmail.com>
2018-11-21 15:04:42 +0900

    前置式に対応した

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,11 +16,14 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{ Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{ Value: node.Value}
+		return nativeBoolToBoolewnObject(node.Value)
 	}
 	return nil
 }
@@ -38,4 +41,37 @@ func nativeBoolToBoolewnObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinuxPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinuxPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
