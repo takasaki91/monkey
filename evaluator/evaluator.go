8ad60193b63e8cc4c5f25ebbbbd573ee8405758c
takasaki91 <y.baton@gmail.com>
2018-11-22 12:08:46 +0900

    真偽地を完璧に使えるようにした

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -83,6 +83,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBoolewnObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBoolewnObject(left != right)
 	default:
 		return NULL
 	}
