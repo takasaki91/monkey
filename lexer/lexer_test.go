277784156fc464d23d7a57b77f2fa394bb1f4056
takasaki91 <y.baton@gmail.com>
2018-12-04 12:35:26 +0900

    ハッシュの字句解析ができるようになった

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,6 +27,7 @@ if ( 5 < 10 ) {
 10 != 0;
 "foobar"
 [1,2];
+{"foo":"bar"}
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -112,6 +113,11 @@ if ( 5 < 10 ) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 	l := New(input)
